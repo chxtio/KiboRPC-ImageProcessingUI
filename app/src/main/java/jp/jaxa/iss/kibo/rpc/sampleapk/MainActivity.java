10d50070c869995650a398d89f2a9b9e0c8bdeae
chxtio <chxtio@gmail.com>
2024-07-16 01:40:04 -0700

    Add pattern matching code

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/MainActivity.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/MainActivity.java
@@ -18,7 +18,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.IOException;
@@ -98,6 +101,18 @@ public class MainActivity extends AppCompatActivity {
         Mat[] templates = loadTemplateImages(imageFileNames);
         this.templates.addAll(Arrays.asList(templates));
         templateAdapter.notifyDataSetChanged();
+
+//        YourService yourService = new YourService();
+        int[] templateMatchCnt = getNumTemplateMatch(templates, mainImage);
+
+        // Handle template match counts
+        for (int i = 0; i < templateMatchCnt.length; i++) {
+            String fileName = imageFileNames.get(i);
+            Log.i(TAG, "Template " + i + " " + fileName + " match count: " + templateMatchCnt[i]);
+        }
+
+        int mostMatchTemplateNum = getMaxIndex(templateMatchCnt);
+        Log.i(TAG, "Best match: " + imageFileNames.get(mostMatchTemplateNum));
     }
 
     private void loadImages() {
@@ -203,4 +218,177 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public int[] getNumTemplateMatch(Mat[] templates, Mat img){
+        // Get the number of template matches
+//        int templateMatchCnt[] = new int[10];
+        int templateMatchCnt[] = new int[templates.length];
+        Mat result = new Mat();
+        for (int tempNum = 0; tempNum < templates.length; tempNum++) {
+//            Log.e(TAG, "Template match: " + tempNum);
+            String fileName = imageFileNames.get(tempNum);
+            Log.e(TAG, "Template matching with: " +  tempNum + " " +  fileName);
+            if (templates[tempNum] == null || templates[tempNum].empty()) {
+                Log.e(TAG, "Template is null or empty at index: " + tempNum);
+                continue; // Skip to the next template
+            }
+
+            // Number of matches
+            int matchCnt = 0;
+            // Coordinates of the matched location
+            List<org.opencv.core.Point> matches = new ArrayList<>();
+
+            // Check if image or template is null
+//            Log.e(TAG, "img test");
+//            if (img.empty()) {
+//                Log.e(TAG, "img is empty");
+//            }
+            if(templates[tempNum].empty()){
+                Log.e(TAG, "template is empty");
+            }
+
+            Mat template = templates[tempNum].clone();
+            Mat targetImg = img.clone();
+
+            // Todo: fix error: (-215) (depth == CV_8U || depth == CV_32F) && type == _templ.type()
+            //  && _img.dims() <= 2 in function cv::matchTemplate
+            // Check if template and image are in compatible format
+            // Check depth
+//            Log.i(TAG, "Saving template: ");
+//            api.saveMatImage(template, "template-test");
+
+//            Log.i(TAG, "Template size: " + template.size());
+//            Log.i(TAG, "Template type: " + template.type() + " (" + CvType.typeToString(template.type()) + ")");
+//            Log.i(TAG, "Template channels (dims): " + template.dims());
+//            Log.i(TAG, "Template channels: " + template.channels());
+//
+//            Log.i(TAG, "Image size: " +targetImg.size());
+//            Log.i(TAG, "Image type: " + targetImg.type() + " (" + CvType.typeToString(img.type()) + ")");
+//            Log.i(TAG, "Image channels (dims): " + targetImg.dims());
+//            Log.i(TAG, "Image channels: " + targetImg.channels());
+
+
+
+            // Pattern matching
+            int widthMin = 20; //[px]
+            int widthMax = 100; //[px]
+            int changeWidth = 5; //[px]
+            int changeAngle = 45; //[px]
+
+            for (int i = widthMin; i <= widthMax; i += changeWidth) {
+                for (int j = 0; j <= 360; j += changeAngle) {
+                    Mat resizedTemp = resizeImg(template, i);
+                    Mat rotResizedTemp = rotImage(resizedTemp, j);
+
+//                    Mat result = new Mat();
+                    Imgproc.matchTemplate(targetImg, rotResizedTemp, result, Imgproc.TM_CCOEFF_NORMED);
+
+                    // Get coordinates with similarity greater than or equal to the threshold
+                    double threshold = 0.75;
+                    Core.MinMaxLocResult mmlr = Core.minMaxLoc(result);
+                    double maxVal = mmlr.maxVal;
+                    if (maxVal >= threshold) {
+                        // Extract only results greater than or equal to the threshold
+                        Mat thresholdedResult = new Mat();
+                        Imgproc.threshold(result, thresholdedResult, threshold, 1.0, Imgproc.THRESH_TOZERO);
+
+                        // Get match counts
+                        for (int y = 0; y < thresholdedResult.rows(); y++) {
+                            for (int x = 0; x < thresholdedResult.cols(); x++) {
+                                if (thresholdedResult.get(y, x)[0] > 0) {
+                                    matches.add(new org.opencv.core.Point(x,y));
+//                                    matchCnt++;
+                                }
+                            }
+                        }
+                        thresholdedResult.release();
+                    }
+                    resizedTemp.release();
+                    rotResizedTemp.release();
+                }
+            }
+
+            // Avoid detecting the same location multiple times
+            List<org.opencv.core.Point> filteredMatches = removeDuplicates(matches);
+            matchCnt += filteredMatches.size();
+            Log.i(TAG, "Template " + tempNum + " match count: " + matchCnt);
+
+            // Number of matches for each template
+            templateMatchCnt[tempNum] = matchCnt;
+
+            template.release();
+            targetImg.release();
+
+        }
+
+        result.release();
+        return templateMatchCnt;
+    }
+
+    // Remove multiple detections
+    public static List<org.opencv.core.Point> removeDuplicates (List<org.opencv.core.Point> points) {
+        double length = 10; //Width 10px
+        List<org.opencv.core.Point> filteredList = new ArrayList<>();
+
+        for(org.opencv.core.Point point : points) {
+            boolean isInclude = false;
+            for (org.opencv.core.Point checkPoint : filteredList) {
+                double distance = calculateDistance(point, checkPoint);
+
+                if(distance <= length){
+                    isInclude = true;
+                    break;
+                }
+            }
+
+            if(!isInclude){
+                filteredList.add(point);
+            }
+        }
+        return filteredList;
+    }
+
+    // Resize image
+    public Mat resizeImg(Mat img, int width){
+        int height = (int) (img.rows() * ((double) width / img.cols()));
+        Mat resizedImg = new Mat();
+        Imgproc.resize(img, resizedImg, new Size(width, height));
+
+        return resizedImg;
+    }
+
+    // Rotate image
+    public Mat rotImage(Mat img, int angle) {
+        org.opencv.core.Point center = new org.opencv.core.Point(img.cols() / 2.0, img.rows() / 2.0);
+        Mat rotatedMat = Imgproc.getRotationMatrix2D(center, angle, 1.0);
+        Mat rotatedImg = new Mat();
+        Imgproc.warpAffine(img, rotatedImg, rotatedMat, img.size());
+
+        return rotatedImg;
+    }
+
+    // Find the distance between two points
+    public static double calculateDistance(org.opencv.core.Point p1, org.opencv.core.Point p2){
+        double dx = p1.x - p2.x;
+        double dy = p1.y - p2.y;
+
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    }
+
+    // Get max value of an array
+    public int getMaxIndex(int[] array) {
+        int max = 0;
+        int maxIndex = 0;
+
+        // Find the index of the element with largest value
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > max) {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
+    }
 }
+
+
